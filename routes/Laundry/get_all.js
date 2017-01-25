26cb315f6a63c6be381327684c87e91f1ac5cee9
David Tschida <dmtschida1@gmail.com>
2017-01-25 15:39:30 -0500

    Reformatting

--- routes/Laundry/get_all.js
+++ routes/Laundry/get_all.js
@@ -3,41 +3,42 @@ var getURL = require('./get_url');
 var request = require('request');
 
 var locations = ["cary", "earhart", "harrison", "hawkins", "hillenbrand", "mccutcheon", "meredith_nw", "meredith_se",
-                 "owen","shreve","tarkington","third","wiley","windsor_duhme","windsor_warren"];
+  "owen", "shreve", "tarkington", "third", "wiley", "windsor_duhme", "windsor_warren"];
 
-function getAllMachines(req){
-  req.logger.info({type:'GET', location:'all'})
-  return new Promise(function(resolve,reject){
+function getAllMachines(req) {
+  req.logger.info({ type: 'GET', location: 'all' })
+  return new Promise(function (resolve, reject) {
     var machines = {}
-    locations.map(function(location){
-    req.redis.exists(location, function(err,exists){
-        if(err){
+    locations.map(function (location) {
+      req.redis.exists(location, function (err, exists) {
+        if (err) {
           req.logger.err('Redis error- ' + err);
-          throw err; 
+          throw err;
+          return; 
         }
-        if(exists == 0 ){
+        if (exists == 0) {
           url = getURL(location);
           url = url.charAt(0).toUpperCase() + url.slice(1);
-          request(url, function(err, response, body){
+          request(url, function (err, response, body) {
             var results = [];
-            if ( !err && response.statusCode == 200 ){
+            if (!err && response.statusCode == 200) {
               results = parseHTML(body);
               machines[location] = results;
-              req.redis.set(location,JSON.stringify(results));
-              req.redis.expire(location,60);
-              if(Object.keys(machines).length === locations.length){
+              req.redis.set(location, JSON.stringify(results));
+              req.redis.expire(location, 60);
+              if (Object.keys(machines).length === locations.length) {
                 resolve(machines);
               }
             }
           });
         } else {
-          req.redis.get(location, function(err,result){
-            if(err) {
+          req.redis.get(location, function (err, result) {
+            if (err) {
               req.logger.err('Redis Error- ' + err);
-              throw err; 
+              throw err;
             }
             machines[location] = JSON.parse(result);
-            if(Object.keys(machines).length === locations.length){
+            if (Object.keys(machines).length === locations.length) {
               resolve(machines);
             }
           })
@@ -48,19 +49,19 @@ function getAllMachines(req){
 }
 
 
-function getAllRoute(req,res){
+function getAllRoute(req, res) {
   console.time("allStart");
-  req.redis.exists('all',function(err,exists){
-    if (err) throw err; 
-    if(exists == 0){
+  req.redis.exists('all', function (err, exists) {
+    if (err) throw err;
+    if (exists == 0) {
       getAllMachines(req)
-        .then(function(machines){
-          req.redis.set('all',JSON.stringify(machines));
-          req.redis.expire('all',60);
+        .then(function (machines) {
+          req.redis.set('all', JSON.stringify(machines));
+          req.redis.expire('all', 60);
           res.json(machines);
         });
     } else {
-      req.redis.get('all',function(err,result){
+      req.redis.get('all', function (err, result) {
         console.timeEnd("allStart");
         var machines = JSON.parse(result);
         res.json(machines);
