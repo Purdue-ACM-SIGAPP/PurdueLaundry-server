5b1d597ff51242ef1f6ba91c356061469306c19a
Jordan Pitlor <jordan@jordanpitlor.com>
2017-03-28 19:44:10 -0400

    get_url now scrapes

--- refreshCache.js
+++ refreshCache.js
@@ -1,27 +1,24 @@
 const request = require('request');
 const parseHTML = require('./routes/Laundry/parse_html');
-const getURL = require('./routes/Laundry/get_url');
-const locations = ["cary", "earhart", "harrison", "hawkins", "hillenbrand", "mccutcheon", "meredith_nw", "meredith_se",
-	"owen", "shreve", "tarkington", "third", "wiley", "windsor_duhme", "windsor_warren"];
+const getLocations = require('./routes/Laundry/get_locations');
 
 
 function updateCache(redis, logger) {
 	logger.info({type: 'refresh'});
 	let machines = {};
-	locations.map(function (location) {
-		let url = getURL(location);
-		url = url.charAt(0).toUpperCase() + url.slice(1);
+	getLocations().then(locations => locations.forEach(function (location) {
+		let url = location.url.charAt(0).toUpperCase() + location.url.slice(1);
 		grabHTML(url)
 			.then(function (results) {
 				machines[location] = results;
-				if (Object.keys(machines).length == locations.length) {
+				if (Object.keys(machines).length === locations.length) {
 					redis.set('all', JSON.stringify(machines));
 					redis.expire('all', 60);
 				}
 				redis.set(location, JSON.stringify(results));
 				redis.expire(location, 60);
 			});
-	});
+	}));
 }
 
 
@@ -29,7 +26,7 @@ function grabHTML(url) {
 	return new Promise(function (resolve, reject) {
 		request(url, function (err, response, body) {
 			let results = [];
-			if (!err && response.statusCode == 200) {
+			if (!err && response.statusCode === 200) {
 				results = parseHTML(body);
 				resolve(results);
 			}
--- routes/Laundry/get_info.js
+++ routes/Laundry/get_info.js
@@ -4,36 +4,37 @@ const parseHTML = require('./parse_html');
 
 module.exports = function (req, res) {
 	req.logger.info({type: 'GET', location: req.params.location});
-	let url = getURL(req.params.location);
-	url = capitalizeFirstLetter(url);
-	if (url == undefined) {
-		req.logger.err("Incorrect URL");
-		res.status(404).send("Error");
-		return;
-	}
-
-	req.redis.exists(req.params.location, function (err, exists) {
-		if (err) {
-			req.logger.err('Redis error- ' + err);
+	getURL(req.params.location).then(function (u) {
+		let url = capitalizeFirstLetter(u);
+		if (url === undefined) {
+			req.logger.err("Incorrect URL");
+			res.status(404).send("Error");
+			return;
 		}
 
-		if (exists == 0) {
-			request(url, function (err, response, body) {
-				let results = [];
-				if (!err && response.statusCode == 200) {
-					results = parseHTML(body);
-				}
-				req.redis.set(req.params.location, JSON.stringify(results));
-				req.redis.expire(req.params.location, 60);
-				res.json(results);
-			});
-		} else {
-			req.redis.get(req.params.location, function (err, result) {
-				if (err) req.logger.err('Redis Error- ' + err);
-				let oldResponse = JSON.parse(result);
-				res.json(oldResponse);
-			})
-		}
+		req.redis.exists(req.params.location, function (err, exists) {
+			if (err) {
+				req.logger.err('Redis error- ' + err);
+			}
+
+			if (exists === 0) {
+				request(url, function (err, response, body) {
+					let results = [];
+					if (!err && response.statusCode === 200) {
+						results = parseHTML(body);
+					}
+					req.redis.set(req.params.location, JSON.stringify(results));
+					req.redis.expire(req.params.location, 60);
+					res.json(results);
+				});
+			} else {
+				req.redis.get(req.params.location, function (err, result) {
+					if (err) req.logger.err('Redis Error- ' + err);
+					let oldResponse = JSON.parse(result);
+					res.json(oldResponse);
+				})
+			}
+		});
 	});
 };
 
