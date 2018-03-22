169233c85cb36d480601ea4f1b8fcaaa9ddc64f8
Jordan Pitlor <jordan@jordanpitlor.com>
2018-03-22 19:52:51 -0400

    express cant do colons

--- server/routes.js
+++ server/routes.js
@@ -96,10 +96,10 @@ module.exports = (app, redis) => {
 	app.get('/v2/location/all', MachineController.getMachines);
 	app.get('/v2/location/:location', MachineController.getMachinesAtLocation);
 
-	app.get('/v2:debug/status', TestController.getPossibleStatuses);
-	app.get('/v2:debug/locations', TestController.getLocations);
-	app.get('/v2:debug/location/all', TestController.getMachines);
-	app.get('/v2:debug/location/:location', TestController.getMachinesAtLocation);
+	app.get('/v2-debug/status', TestController.getPossibleStatuses);
+	app.get('/v2-debug/locations', TestController.getLocations);
+	app.get('/v2-debug/location/all', TestController.getMachines);
+	app.get('/v2-debug/location/:location', TestController.getMachinesAtLocation);
 
 	app.all('*', fourOhFour);
 };
